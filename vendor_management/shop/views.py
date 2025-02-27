1a1ac6f0b3b4e44469e22151cb0314cfe510ed41
atishayjain2003 <atishayjain2003@gmail.com>
2025-02-27 19:38:42 +0530

    User Registration added, djangoauthtoken implemented

--- vendor_management/shop/views.py
+++ vendor_management/shop/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from django.contrib.auth.models import User
+from rest_framework.authtoken.models import Token
 
-# Create your views here.
+class RegisterUserView(APIView):
+    def post(self, request):
+        username = request.data.get('username')
+        password = request.data.get('password')
+
+        if not username or not password:
+            return Response({'error': 'Please provide both username and password'})
+
+        if User.objects.filter(username=username).exists():
+            return Response({'error': 'Username already exists'})
+
+        user = User.objects.create_user(username=username, password=password)
+        token, _ = Token.objects.get_or_create(user=user)
+
+        return Response({'message': 'The user has been created successfully', 'token': token.key})
--- vendor_management/vendor_management/urls.py
+++ vendor_management/vendor_management/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from shop.views import RegisterUserView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('home', RegisterUserView.as_view()),
 ]
